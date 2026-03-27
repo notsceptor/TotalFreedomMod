b1efda86dca8cb13deb00a2b43829c1c18b5f27b
grntbg <13023526+aokod@users.noreply.github.com>
2026-03-27 13:59:36 -0500

    fix cmd args for world + spawnmob crash bug

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_entitywipe.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_entitywipe.java
@@ -2,18 +2,35 @@ package me.totalfreedom.totalfreedommod.command;
 
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, permission = "tfm.server.entitywipe")
-@CommandParameters(description = "Remove various server entities that may cause lag, such as dropped items, minecarts, and boats.", usage = "/<command> <carts>", aliases = "ew,rd")
+@CommandParameters(description = "Remove various server entities that may cause lag, such as dropped items, minecarts, and boats.", usage = "/<command> [world]", aliases = "ew,rd")
 public class Command_entitywipe extends FreedomCommand
 {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        if (args.length >= 1)
+        {
+            World world = Bukkit.getWorld(args[0]);
+            if (world == null)
+            {
+                msg("World \"" + args[0] + "\" not found.", ChatColor.RED);
+                return true;
+            }
+            FUtil.adminAction(sender.getName(), "Removing entities in world " + world.getName(), true);
+            int removed = plugin.ew.wipeEntities(world, true);
+            msg(removed + " entities removed from \"" + world.getName() + "\".");
+            return true;
+        }
+
         FUtil.adminAction(sender.getName(), "Removing all server entities.", true);
         msg((plugin.ew.wipeEntities(true)) + " entities removed.");
 
