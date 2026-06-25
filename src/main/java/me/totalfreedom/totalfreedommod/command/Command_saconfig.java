7d2f8382a69a88a5d2b1cbd79c83710a03fd291d
super <awesomegamer156@gmail.com>
2026-06-25 19:33:25 -0400

    Remove kicknoob and add a hyphen to saconfig

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_saconfig.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_saconfig.java
@@ -169,7 +169,7 @@ public class Command_saconfig extends FreedomCommand
         else // Existing admin
         {
             player.setOp(true);
-            FUtil.adminAction(sender.getName(), "Readding " + admin.getName() + " to the admin list", true);
+            FUtil.adminAction(sender.getName(), "Re-adding " + admin.getName() + " to the admin list", true);
 
             if (player != null)
             {
