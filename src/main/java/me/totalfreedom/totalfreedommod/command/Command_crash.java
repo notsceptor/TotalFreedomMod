38ac389f0caa6cd3055e1b7ba0f90a12b0f9c30f
smartnt <29356843+smartnt@users.noreply.github.com>
2026-07-01 18:02:10 -0400

    Special thing for the seniors (actually for real this time)

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_crash.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_crash.java
@@ -1,4 +1,51 @@
 package me.totalfreedom.totalfreedommod.command;
 
-public class Command_crash {
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Particle;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH, permission = "tfm.admin.senior.fuckup")
+@CommandParameters(description = "Crashes the specified player", usage = "/<command> <player>", aliases = "fuckup")
+public class Command_crash extends FreedomCommand
+{
+    @CommandDispatchTarget(pattern = "<player:Player>")
+    public boolean crash(CommandContext ctx, Player player)
+    {
+        player.spawnParticle(
+                Particle.ASH,
+                player.getLocation(),
+                1_000_000_000,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                null,
+                true
+        );
+
+        Component c = Component.text("GET ABSOLUTELY FUCKED!");
+        for (int i = 0; i < 50; ++i)
+            c = Component.translatable("%1$s%1$s%1$s", "%1$s%1$s%1$s", c);
+
+        player.sendMessage(c);
+
+        sender.sendMessage(
+                Component.text("Crashed ", NamedTextColor.GRAY)
+                        .append(Component.text(player.getName(), NamedTextColor.GRAY))
+                        .append(Component.text(".", NamedTextColor.GRAY))
+        );
+
+        return true;
+    }
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        return false;
+    }
 }
