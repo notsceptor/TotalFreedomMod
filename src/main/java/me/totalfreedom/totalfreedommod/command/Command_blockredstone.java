9bfa55c690cf1a31436c6751b8dd3f61ec60591b
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-03 16:08:24 -0600

    /blockredstone: use new command system, allow for manually setting the parameter

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_blockredstone.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_blockredstone.java
@@ -6,36 +6,50 @@ import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, permission = "tfm.admin.blockredstone")
-@CommandParameters(description = "Blocks redstone on the server.", usage = "/<command>", aliases = "bre")
+@CommandParameters(description = "Blocks redstone on the server.", usage = "/<command> <value>", aliases = "bre")
 public class Command_blockredstone extends FreedomCommand
 {
+    private BukkitTask autoUnblock = null;
 
+    @CommandDispatchTarget
+    public boolean toggle(CommandContext ctx)
+    {
+        return setValue(ctx, ConfigEntry.ALLOW_REDSTONE.getBoolean());
+    }
+
+    @CommandDispatchTarget(pattern = "<value:Boolean>")
+    public boolean setValue(CommandContext ctx, Boolean newValue)
+    {
+        FUtil.adminAction(ctx.getSender().getName(), (newValue ? "B" : "Unb") + "locking all redstone", true);
+        ConfigEntry.ALLOW_REDSTONE.setBoolean(!newValue);
+
+        if (autoUnblock != null)
+        {
+            autoUnblock.cancel();
+        }
+
+        if (newValue)
+        {
+            // This is stupid as hell
+            autoUnblock = server.getScheduler().runTaskLater(plugin, () ->
+            {
+                if (!ConfigEntry.ALLOW_REDSTONE.getBoolean())
+                {
+                    FUtil.adminAction("TotalFreedom", "Unblocking all redstone", false);
+                    ConfigEntry.ALLOW_REDSTONE.setBoolean(true);
+                }
+            }, 6000L);
+        }
+
+        return true;
+    }
+
+    @Override
     public boolean run(final CommandSender sender, final Player playerSender, final Command cmd, final String commandLabel, final String[] args, final boolean senderIsConsole)
     {
-        if (ConfigEntry.ALLOW_REDSTONE.getBoolean())
-        {
-            ConfigEntry.ALLOW_REDSTONE.setBoolean(false);
-            FUtil.adminAction(sender.getName(), "Blocking all redstone", true);
-            new BukkitRunnable()
-            {
-                public void run()
-                {
-                    if (!ConfigEntry.ALLOW_REDSTONE.getBoolean())
-                    {
-                        FUtil.adminAction("TotalFreedom", "Unblocking all redstone", false);
-                        ConfigEntry.ALLOW_REDSTONE.setBoolean(true);
-                    }
-                }
-            }.runTaskLater(plugin, 6000L);
-        }
-        else
-        {
-            ConfigEntry.ALLOW_REDSTONE.setBoolean(true);
-            FUtil.adminAction(sender.getName(), "Unblocking all redstone", true);
-        }
-        return true;
+        return false;
     }
 }
