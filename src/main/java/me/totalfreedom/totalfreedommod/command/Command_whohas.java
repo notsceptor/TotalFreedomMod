12cd223e017a36afaaa1d3f02c65a597f8783b2d
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-03 22:09:34 -0600

    /whohas: fallback onto /data get if essentials is not installed or enabled

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_whohas.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_whohas.java
@@ -43,7 +43,9 @@ public class Command_whohas extends FreedomCommand
                                                 .colorIfAbsent(NamedTextColor.WHITE)
                                                 .hoverEvent(HoverEvent.showText(Component.text(player.getName())))
                                                 .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.SUGGEST_COMMAND,
-                                                        ClickEvent.Payload.string("/invsee " + player.getName())))).toList())));
+                                                        ClickEvent.Payload.string(plugin.esb.isEssentialsEnabled() ?
+                                                                  "/invsee " + player.getName() :
+                                                                  "/data get entity " + player.getUniqueId() + " Inventory")))).toList())));
 
                 return true;
             }
