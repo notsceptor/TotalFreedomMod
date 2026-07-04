1849759980c181835b3aa8419a173522583df20e
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-04 14:49:36 -0600

    /rank: use new command system

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_rank.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_rank.java
@@ -13,44 +13,20 @@ import org.bukkit.entity.Player;
 @CommandParameters(description = "Shows ranks", usage = "/<command> [player]")
 public class Command_rank extends FreedomCommand
 {
-
-    @Override
-    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    @CommandDispatchTarget
+    public boolean querySelfOrAll(CommandContext ctx)
     {
-        if (isConsole() && args.length == 0)
+        if (ctx.isSenderConsole())
         {
-            for (Player player : server.getOnlinePlayers())
-            {
-                msg(message(player));
-            }
+            server.getOnlinePlayers().forEach(player -> queryPlayer(ctx, player));
             return true;
         }
 
-        if (args.length == 0)
-        {
-            msg(message(playerSender));
-            return true;
-        }
-
-        if (args.length > 1)
-        {
-            return false;
-        }
-
-        final Player player = getPlayer(args[0]);
-
-        if (player == null)
-        {
-            msg(FreedomCommand.PLAYER_NOT_FOUND);
-            return true;
-        }
-
-        msg(message(player));
-
-        return true;
+        return queryPlayer(ctx, ctx.getPlayerSender());
     }
 
-    public Component message(Player player)
+    @CommandDispatchTarget(pattern = "<player:Player>")
+    public boolean queryPlayer(CommandContext ctx, Player player)
     {
         Displayable display = plugin.rm.getDisplay(player);
         Rank rank = plugin.rm.getRank(player);
@@ -65,6 +41,13 @@ public class Command_rank extends FreedomCommand
                     .append(Component.text(")", NamedTextColor.AQUA));
         }
 
-        return result;
+        msg(ctx.getSender(), result);
+        return true;
+    }
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        return false;
     }
 }
