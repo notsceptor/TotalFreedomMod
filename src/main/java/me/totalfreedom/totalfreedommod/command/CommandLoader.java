41dcb394435adbfa52781cf0229611792bfa9456
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-04 21:24:54 -0600

    /plugincontrol: use new command system

--- src/main/java/me/totalfreedom/totalfreedommod/command/CommandLoader.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/CommandLoader.java
@@ -45,6 +45,7 @@ public class CommandLoader extends FreedomService
         handler.registerArgumentResolver(EntityTypeArgumentResolver.class);
         handler.registerArgumentResolver(MaterialArgumentResolver.class);
         handler.registerArgumentResolver(MaterialQueryArgumentProvider.class);
+        handler.registerArgumentResolver(PluginArgumentResolver.class);
         handler.registerArgumentResolver(BooleanArgumentResolver.class);
         handler.registerArgumentResolver(IntegerArgumentResolver.class);
         handler.registerArgumentResolver(DoubleArgumentResolver.class);
