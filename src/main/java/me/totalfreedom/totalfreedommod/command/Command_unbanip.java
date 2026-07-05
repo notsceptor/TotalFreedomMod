440ab8978ea6beeb448e73d5633d743d8a78e5f9
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-05 00:10:30 -0600

    /unbanip: use new command system

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_unbanip.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_unbanip.java
@@ -1,31 +1,40 @@
 package me.totalfreedom.totalfreedommod.command;
 
-import me.totalfreedom.totalfreedommod.banning.Ban;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.net.InetAddress;
+import java.util.List;
+
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, permission = "tfm.admin.ban")
 @CommandParameters(description = "Unbans an IP address.", usage = "/<command> <ip>")
 public class Command_unbanip extends FreedomCommand
 {
+    @CommandDispatchTarget(pattern = "<ip:IPs>")
+    public boolean unbanIP(CommandContext ctx, List<InetAddress> ips)
+    {
+        // So right now, this is coded to only take one IP address. In the future, this could probably be rewritten to
+        //  accept multiple addresses.
+        final String ip = ips.getFirst().getHostAddress();
+
+        if (!plugin.bm.isIpBanned(ip))
+        {
+            msg(ctx.getSender(), "That IP address is not banned.");
+            return true;
+        }
+
+        FUtil.adminAction(ctx.getSender().getName(), "Unbanning IP " + FUtil.sanitizeIp(ctx.getSender(), ip), true);
+        plugin.bm.unbanIp(ip);
+
+        return true;
+    }
+
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length != 1)
-        {
-            return false;
-        }
-        Ban ban = plugin.bm.getByIp(args[0]);
-        if (ban == null)
-        {
-            msg("That IP address is not banned.");
-            return true;
-        }
-        plugin.bm.removeBan(ban);
-        FUtil.adminAction(sender.getName(), "Unbanning IP " + FUtil.sanitizeIp(sender, args[0]), true);
-        return true;
+        return false;
     }
 }
