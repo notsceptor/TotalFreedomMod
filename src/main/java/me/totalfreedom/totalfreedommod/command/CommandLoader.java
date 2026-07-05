9a9b0b0279dd58868e0adacaac90f26b911ed661
VideoGameSmash12 <videogamesm12@gmail.com>
2026-07-05 00:33:09 -0600

    /myadmin: use new command system

--- src/main/java/me/totalfreedom/totalfreedommod/command/CommandLoader.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/CommandLoader.java
@@ -46,6 +46,7 @@ public class CommandLoader extends FreedomService
         handler.registerArgumentResolver(MaterialArgumentResolver.class);
         handler.registerArgumentResolver(MaterialQueryArgumentProvider.class);
         handler.registerArgumentResolver(PluginArgumentResolver.class);
+        handler.registerArgumentResolver(InetAddressResolver.class);
         handler.registerArgumentResolver(InetAddressListResolver.class);
         handler.registerArgumentResolver(BooleanArgumentResolver.class);
         handler.registerArgumentResolver(IntegerArgumentResolver.class);
--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_unbanip.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_unbanip.java
@@ -7,27 +7,22 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.net.InetAddress;
-import java.util.List;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, permission = "tfm.admin.ban")
 @CommandParameters(description = "Unbans an IP address.", usage = "/<command> <ip>")
 public class Command_unbanip extends FreedomCommand
 {
-    @CommandDispatchTarget(pattern = "<ip:IPs>")
-    public boolean unbanIP(CommandContext ctx, List<InetAddress> ips)
+    @CommandDispatchTarget(pattern = "<ip:IP>")
+    public boolean unbanIP(CommandContext ctx, InetAddress ip)
     {
-        // So right now, this is coded to only take one IP address. In the future, this could probably be rewritten to
-        //  accept multiple addresses.
-        final String ip = ips.getFirst().getHostAddress();
-
-        if (!plugin.bm.isIpBanned(ip))
+        if (!plugin.bm.isIpBanned(ip.getHostAddress()))
         {
             msg(ctx.getSender(), "That IP address is not banned.");
             return true;
         }
 
-        FUtil.adminAction(ctx.getSender().getName(), "Unbanning IP " + FUtil.sanitizeIp(ctx.getSender(), ip), true);
-        plugin.bm.unbanIp(ip);
+        FUtil.adminAction(ctx.getSender().getName(), "Unbanning IP " + FUtil.sanitizeIp(ctx.getSender(), ip.getHostAddress()), true);
+        plugin.bm.unbanIp(ip.getHostAddress());
 
         return true;
     }
