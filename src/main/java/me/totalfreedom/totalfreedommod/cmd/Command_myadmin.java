e806b940d6e329cf33808ec3b26f41a721b1a569
shrimp <luke560@users.noreply.github.com>
2026-07-18 15:20:52 -0600

    Use MiniMessage Formatter.number instead of String.format in myadmin

--- src/main/java/me/totalfreedom/totalfreedommod/cmd/Command_myadmin.java
+++ src/main/java/me/totalfreedom/totalfreedommod/cmd/Command_myadmin.java
@@ -6,6 +6,7 @@ import java.util.List;
 import me.totalfreedom.totalfreedommod.admin.Admin;
 import me.totalfreedom.totalfreedommod.cmd.internal.annotation.*;
 import me.totalfreedom.totalfreedommod.rank.Rank;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
 import org.bukkit.entity.Player;
 
 @Permission(level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME, permission = "tfm.admin.myadmin")
@@ -44,7 +45,7 @@ public class Command_myadmin extends FCommand
         ips.clear();
         ips.add(player.getAddress().getAddress().getHostName());
 
-        msg(player, String.format("<gray>%d IP address(es) were removed.", count - 1));
+        msg(player, "<gray><count> IP address(es) were removed.", Formatter.number("count", count - 1));
     }
 
     @Callback
