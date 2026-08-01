cba736f813759388d78c81feb7bb720b45fc0b52
shrimp <luke560@users.noreply.github.com>
2026-08-01 10:04:49 -0600

    Add /signspy command and service

---
+++ src/main/java/me/totalfreedom/totalfreedommod/cmd/Command_signspy.java
@@ -0,0 +1,26 @@
+package me.totalfreedom.totalfreedommod.cmd;
+
+import org.bukkit.entity.Player;
+
+import me.totalfreedom.totalfreedommod.cmd.internal.annotation.*;
+import me.totalfreedom.totalfreedommod.player.PlayerData;
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
+
+@Command(name = "signspy", description = "Spy on sign edits", usage = "/signspy", aliases = {"sspy"})
+@Permission(permission = "tfm.admin.signspy", level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
+public class Command_signspy extends FCommand
+{
+    @Callback
+    public void toggle(final Player player)
+    {
+        final PlayerData data = plugin().pl.getData(player);
+        data.setSignSpy(!data.isSignSpy());
+        plugin().pl.saveAsync();
+        msg(
+            player,
+            "<gray>SignSpy <status:enabled:disabled>.",
+            Formatter.booleanChoice("status", data.isSignSpy())
+        );
+    }
+}
