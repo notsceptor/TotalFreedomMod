8acb771e3feb221a8076e4c086585d6d5c36d812
shrimp <luke560@users.noreply.github.com>
2026-08-17 06:44:05 -0600

    fix /jumppads

--- src/main/java/me/totalfreedom/totalfreedommod/cmd/Command_jumppads.java
+++ src/main/java/me/totalfreedom/totalfreedommod/cmd/Command_jumppads.java
@@ -11,11 +11,11 @@ import org.bukkit.command.CommandSender;
 import java.util.stream.Stream;
 
 @Permission(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, permission = "tfm.fun.jumppads")
-@Command(name = "jumppads", description = "Manage jumppads", usage = "/<command> <<on | off> | info | mode <mode> | strength <strength>>", aliases = "launchpads,jp")
+@Command(name = "jumppads", description = "Manage jumppads", usage = "/<command> <<on | off> | info | mode <mode> | strength <strength>>", aliases = {"launchpads", "jp"})
 public class Command_jumppads extends FCommand
 {
     @Callback
-    public void setEnabled(CommandSender sender, Boolean status)
+    public void setEnabled(CommandSender sender, @Resolve("Boolean") boolean status)
     {
         adminAction(sender, "<aqua><choice:Enabling:Disabling> Jumppads", Formatter.booleanChoice("choice", status));
         plugin().jp.setMode(status ? Jumppads.JumpPadMode.NORMAL : Jumppads.JumpPadMode.OFF);
@@ -60,7 +60,7 @@ public class Command_jumppads extends FCommand
 
         adminAction(
                     sender, 
-                    "<green><status:Setting:Enabling and setting> Jumppads to <mode>.", 
+                    "<aqua><status:Setting:Enabling and setting> Jumppads to <mode>.", 
                     Formatter.booleanChoice("status", plugin().jp.getMode().isOn()), 
                     Placeholder.unparsed("mode", mode.getLabel()));
 
